2754a8188a7ffac83d4684af84005bef9255bc36
Moisés <110428698+moisespezao@users.noreply.github.com>
2022-08-03 17:13:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const checkBox = document.getElementById('inputSwitch');
-checkBox.addEventListener("change", updateBackground);
-updateBackground();
+const sun = document.getElementById('sun');
+const moon = document.getElementById('moon');
+checkBox.addEventListener("change", setTheme);
+setTheme();
 
-function updateBackground() {
+function setTheme() {
   document.body.classList = checkBox.checked ? "dark" : "clear";
+  sun.style.filter = checkBox.checked ? "invert(0%) sepia(100%) saturate(18%) hue-rotate(72deg) brightness(101%) contrast(103%)" : "invert(0%) sepia(100%) saturate(18%) hue-rotate(72deg) brightness(101%) contrast(103%)";
+  moon.style.filter = checkBox.checked ? "invert(85%) sepia(76%) saturate(2699%) hue-rotate(173deg) brightness(97%) contrast(103%)" : "invert(85%) sepia(76%) saturate(2699%) hue-rotate(173deg) brightness(97%) contrast(103%)";
 }
